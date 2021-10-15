4815bdccfdaacd65e58a0377dc9cef58cc0b2010
Alfonso Vallejo <ricardovallejo@seven4n.com>
2021-10-15 11:15:49 -0500

    Data Builder Pattern
    Added class to create Data Builder pattern

---
+++ src/main/java/data/builder/RegisterUserDataBuilder.java
@@ -0,0 +1,46 @@
+package data.builder;
+
+import com.github.javafaker.Faker;
+import data.factory.RegisterUserDataFactory;
+import model.RegisterUserRequest;
+
+public class RegisterUserDataBuilder {
+
+    private static final Faker FAKER = new Faker();
+    private RegisterUserRequest userRequest;
+
+    private RegisterUserDataBuilder() {
+        userRequest = new RegisterUserRequest();
+        this.userRequest = RegisterUserDataFactory.validDefaultUser();
+    }
+
+    public static RegisterUserDataBuilder registerUserDataBuilder(){
+        return new RegisterUserDataBuilder();
+    }
+
+    public RegisterUserDataBuilder withPassword(String password) {
+        this.userRequest.setPassword(password);
+        return this;
+    }
+
+    public RegisterUserDataBuilder withEmail(String email) {
+        this.userRequest.setEmail(email);
+        return this;
+    }
+
+    public RegisterUserDataBuilder withEmptyEmail() {
+        this.userRequest.setEmail("");
+        return this;
+    }
+
+    public RegisterUserDataBuilder withEmptyPassword() {
+        this.userRequest.setPassword("");
+        return this;
+    }
+
+    public RegisterUserDataBuilder withWrongEmail() {
+        this.userRequest.setEmail(FAKER.internet().emailAddress());
+        return this;
+    }
+
+}
